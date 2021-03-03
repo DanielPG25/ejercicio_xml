0b8867f517b71f6fc899e6949904801596e238f1
Daniel Parrales García <dannyparrales16@gmail.com>
2021-03-03 14:07:01 +0100

    Añadida la función para buscar por número de jugadores y filtrar por la clasificación de edad y apartado 5

--- funciones_wii.py
+++ funciones_wii.py
@@ -45,4 +45,52 @@ def BuscarPorGenero(genero,fichero):
 	for a in lista2:
 		if genero in a.xpath("./genre/text()"):
 			lista.append(a.xpath("./description/text()"))
-	return lista					
+	return lista
+
+def BuscarPorJugadores(jugadores,fichero):
+	lista=[]
+	lista2 = fichero.xpath("/menu/game")
+	for a in lista2:
+		dicc={}
+		dicc['nombre']=a.xpath("./description/text()")
+		dicc['jug']=a.xpath("./player/text()")
+		dicc['edad']=a.xpath("./rating/text()")
+		lista.append(dicc)
+	lista3=[]
+	for b in lista:
+		if jugadores == 1:
+			dicc2={}
+			dicc2['nombre']=b.get('nombre')[0]
+			dicc2['jug']=b.get('jug')[0]
+			dicc2['edad']=b.get('edad')[0]
+			lista3.append(dicc2)
+		elif b.get('jug')[0] != '1 Player':
+			if jugadores <= int(b.get('jug')[0][2]) and jugadores < 10:
+				dicc2={}
+				dicc2['nombre']=b.get('nombre')[0]
+				dicc2['jug']=b.get('jug')[0]
+				dicc2['edad']=b.get('edad')[0]
+				lista3.append(dicc2)
+			if jugadores <= 10 and b.get('jug')[0] == '1-10 Players':
+				dicc2={}
+				dicc2['nombre']=b.get('nombre')[0]
+				dicc2['jug']=b.get('jug')[0]
+				dicc2['edad']=b.get('edad')[0]
+				lista3.append(dicc2)
+			if jugadores <= 12 and b.get('jug')[0] == '1-12 Players':
+				dicc2={}
+				dicc2['nombre']=b.get('nombre')[0]
+				dicc2['jug']=b.get('jug')[0]
+				dicc2['edad']=b.get('edad')[0]
+				lista3.append(dicc2)					
+	return lista3
+
+def FiltrarPorEdad(edad,listadicc):
+	lista=[]
+	for a in listadicc:
+		if edad == "Everyone":
+			if a.get('edad')=="ESRB - E (Everyone)":
+				lista.append(a.get('nombre'))
+		elif edad in a.get('edad'):
+			lista.append(a.get('nombre'))
+	return lista						
--- juegos_wii.py
+++ juegos_wii.py
@@ -14,6 +14,9 @@ while opcion != 6:
 			print(a)
 		print()
 		opcion = Menu()
+		while opcion < 1 or opcion > 6:
+			print("Esa no es una opción válida")
+			opcion = Menu()
 	if opcion == 2:
 		print("Las regiones disponibles son USA, Europe y Japan.")
 		print()
@@ -22,27 +25,57 @@ while opcion != 6:
 		juegos = ContarRegion(region,fichero)
 		print(f"La región {region} tiene un total de {juegos} juegos")
 		print()
-		opcion=Menu()	
+		opcion=Menu()
+		while opcion < 1 or opcion > 6:
+			print("Esa no es una opción válida")
+			opcion = Menu()	
 	if opcion == 3:
 		print("Ahora buscaremos los juegos que empiecen por la cadena introducida a continuación")
 		cadena = input("Dime la cadena (empiece por mayúscula): ")
-		juegos = BuscarPorCadena(cadena,fichero)
+		juegosa = BuscarPorCadena(cadena,fichero)
 		print()
-		for a in juegos:
+		for a in juegosa:
 			print(f"El juego {a.get('nombre')[0]} fue desarrollado por {a.get('desarrolladora')[0]} en el año {a.get('año')[0]}.")
 		print()
 		opcion=Menu()
+		while opcion < 1 or opcion > 6:
+			print("Esa no es una opción válida")
+			opcion = Menu()
 	if opcion == 4:
 		print("Ahora buscaremos juegos a partir de su género.")
 		print("Esta es una lista de los genéros más importantes para poder buscar:")
 		print("Action, Shooter, Driving, Party, Adventure, Puzzle, Fitness, Rhythm, Board Game, Tennis, Hockey, Golf, Role-Playing, Simulation, Fishing, Flying, Quiz, Platform, Soccer, Football, etc")
 		genero = input("Elige uno de los géneros anteriores: ")
-		juegos = BuscarPorGenero(genero,fichero)
+		juegosb = BuscarPorGenero(genero,fichero)
 		print()
 		print(f"Los juegos del género {genero} son:")
 		print()
-		for a in juegos:
+		for a in juegosb:
 			print(a[0])
 		print()
-		opcion=Menu()			
+		opcion=Menu()
+		while opcion < 1 or opcion > 6:
+			print("Esa no es una opción válida")
+			opcion = Menu()
+	if opcion == 5:
+		jugadores=int(input("Introduce el número de jugadores (máximo 12): "))
+		juegosc=BuscarPorJugadores(jugadores,fichero)
+		print()
+		print("Las clasificaciones por edad son las siguientes:")
+		print("Everyone, E10+, Teen, Kids to Adults, Mature")
+		print()
+		edad = input("Elige una de las anteriores (tal y como están escritas arriba): ")
+		print()
+		print("Si la búsqueda por clasificación de edad está vacía, es que no hay ningún juego con esa clasificación para los jugadores que ha pedido.")
+		print()
+		juegosfiltrados = FiltrarPorEdad(edad,juegosc)
+		print()
+		print("Los juegos con esa clasificacion por edad son: ")
+		for a in juegosfiltrados:
+			print(a)
+		opcion=Menu()
+		while opcion < 1 or opcion > 6:
+			print("Esa no es una opción válida")
+			opcion = Menu()
+print("Programa finalizado")						
 		
